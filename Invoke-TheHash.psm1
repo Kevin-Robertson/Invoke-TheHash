af2505ca5973813ba0658b61f61f5acfc09e2e2c
Kevin Robertson <robertsonk@gmail.com>
2017-01-02 20:29:50 -0500

    Initial commit

---
+++ Invoke-TheHash.psm1
@@ -0,0 +1,10 @@
+<#
+.SYNOPSIS
+Invoke-TheHash - PowerShell Pass The Hash Utils
+
+.LINK
+https://github.com/Kevin-Robertson/Invoke-TheHash
+#>
+Import-Module $PWD\Invoke-TheHash.ps1
+Import-Module $PWD\Invoke-SMBExec.ps1
+Import-Module $PWD\Invoke-WMIExec.ps1
